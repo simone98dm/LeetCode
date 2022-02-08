377c87d10a8f7161fce26f98492b581beb837930
simone98dm <simone.dalmas@outlook.it>
2022-02-08 20:47:52 +0100

    Add longest prefix solution

---
+++ LeetCode.Solves/LongestPrefix/LogestPrefix.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Solves.Base;
+
+namespace LeetCode.Solves.LongestPrefix
+{
+    public class LogestPrefix : BaseSolution<string[], string>
+    {
+        public override string Solve(string[] param)
+        {
+            string s = string.Empty;
+            string first = param[0];
+
+            var all = param.Where(x => x != first);
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                s += first[i];
+                if (!all.All(word => word.StartsWith(s)))
+                {
+                    return s.Remove(s.Length - 1);
+                }
+            }
+
+            return s;
+        }
+    }
+}
