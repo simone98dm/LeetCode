1461e42e47d1f8cbdfef58f8e312a3dd73e81427
simone98dm <simone.dalmas@outlook.it>
2022-02-08 23:12:08 +0100

    Add solution strStr()

---
+++ LeetCode.Solves/strStr/strStr.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Solves.Base;
+
+namespace LeetCode.Solves.strStr
+{
+    public class strStr : BaseSolution<string, string, int>
+    {
+        public override int Solve(string param1, string param2)
+        {
+            if (string.IsNullOrEmpty(param2))
+            {
+                return 0;
+            }
+
+            if (param1.Length < param2.Length)
+            {
+                return -1;
+            }
+
+            char[] vs = param2.ToArray();
+            for (int i = 0; i < param1.Length; i++)
+            {
+                if (param1[i] == vs[0] && (param1.Length - i >= param2.Length))
+                {
+                    if (param1.Substring(i, param2.Length) == param2)
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
