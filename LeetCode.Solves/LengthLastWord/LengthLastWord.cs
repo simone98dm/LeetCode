b3c7f7affa6474dda8c05ddb7fd2ebadf590c369
simone98dm <simone.dalmas@outlook.it>
2022-02-14 21:12:56 +0100

    Solve last word length

---
+++ LeetCode.Solves/LengthLastWord/LengthLastWord.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Solves.Base;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solves.LengthLastWord
+{
+    public class LengthLastWord : BaseSolution<string, int>
+    {
+        public override int Solve(string s)
+        {
+            string t = s.Trim();
+            if (!t.Contains(' '))
+            {
+                return t.Length;
+            }
+            var sl = t.Split(' ');
+            return sl[sl.Length - 1].Length;
+        }
+    }
+}
