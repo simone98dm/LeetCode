0e0be939337e364b94c3d916e9c9dbb1c9474edf
simone98dm <simone.dalmas@outlook.it>
2022-02-19 07:34:06 +0100

    Solve max subarray

---
+++ LeetCode.Solves/MaximumSubarray/MaximumSubarray.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Solves.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solves.MaximumSubarray
+{
+    public class MaximumSubarray : BaseSolution<int[], int>
+    {
+        public override int Solve(int[] nums)
+        {
+            int maxSummed = int.MinValue;
+            int maxSummedCurrent = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                maxSummedCurrent += nums[i];
+                if (maxSummedCurrent > maxSummed)
+                {
+                    maxSummed = maxSummedCurrent;
+                }
+
+                if (maxSummedCurrent < 0)
+                {
+                    maxSummedCurrent = 0;
+                }
+            }
+            return maxSummed;
+        }
+    }
+}
