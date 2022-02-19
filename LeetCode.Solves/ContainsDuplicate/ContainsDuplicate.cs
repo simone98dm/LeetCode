bd3d752f3346ba6718b4dd0d6e9543cdff3042fd
simone98dm <simone.dalmas@outlook.it>
2022-02-19 07:46:41 +0100

    Solve contins duplicate

---
+++ LeetCode.Solves/ContainsDuplicate/ContainsDuplicate.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.Solves.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solves.ContainsDuplicate
+{
+    public class ContainsDuplicate : BaseSolution<int[], bool>
+    {
+        public override bool Solve(int[] nums)
+        {
+            return nums.GroupBy(x => x).Any(x => x.Count() > 1);
+        }
+    }
+}
