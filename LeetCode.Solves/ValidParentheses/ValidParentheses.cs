48e90d146799e5576f204520ac38ba0d23a4d16f
simone98dm <simone.dalmas@outlook.it>
2022-02-25 22:35:08 +0100

    Solve valid parentheses

---
+++ LeetCode.Solves/ValidParentheses/ValidParentheses.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode.Solves.Base;
+
+namespace LeetCode.Solves.ValidParentheses
+{
+    public class ValidParentheses : BaseSolution<string, bool>
+    {
+        public override bool Solve(string s)
+        {
+            List<char> list = new List<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+                {
+                    list.Add(s[i]);
+                }
+                else
+                {
+                    if (list.Count == 0)
+                    {
+                        return false;
+                    }
+                    var top = list.Last();
+                    if (s[i] == ')' && top == '(' ||
+                        s[i] == ']' && top == '[' ||
+                        s[i] == '}' && top == '{')
+                    {
+                        list.RemoveAt(list.Count - 1);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (list.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
