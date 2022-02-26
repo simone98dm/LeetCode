abe4936c45ac3aec300b232d6dc49e2cb3e27847
simone98dm <simone.dalmas@outlook.it>
2022-02-26 10:46:25 +0100

    Solve compare version number

---
+++ LeetCode.Solves/CompareVersionNumbers/CompareVersionNumbers.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Solves.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solves.CompareVersionNumbers
+{
+    public class CompareVersionNumbers : BaseSolution<string, string, int>
+    {
+        public override int Solve(string version1, string version2)
+        {
+            var c1 = version1.Split(".");
+            var c2 = version2.Split(".");
+            int len = c1.Length;
+            if (c2.Length > len)
+            {
+                len = c2.Length;
+            }
+            for (int i = 0; i < len; i++)
+            {
+                int v1 = 0;
+                if (i < c1.Length)
+                {
+                    v1 = int.Parse(c1[i]);
+                }
+                var v2 = 0;
+                if (i < c2.Length)
+                {
+                    v2 = int.Parse(c2[i]);
+                }
+                if (v1 < v2)
+                {
+                    return 1;
+                }
+
+                if (v1 > v2)
+                {
+                    return -1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
